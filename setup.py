748e3551f38987241681ac732b531036d20373cf
Seungha Son <linuxias@gmail.com>
2023-06-02 08:52:20 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name='TDA',
+    version='0.1',
+    description="TDA is Tools for Data Analsis easier.",
+    author="Seungha Son",
+    author_email="linuxias@gmail.com",
+    license="MIT",
+    url="https://github.com/linuxias/TDA",
+    packages=setuptools.find_packages(),
+    keywords=["Data Analysis", "Supporter"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.10"
+    ],
+)
